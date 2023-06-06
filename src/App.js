1d9e97a69e0a07b93766f132c2ac3a05ac5a2e34
ygw212 <wangygca@gmail.com>
2023-06-05 22:25:53 -0700

    update
    word cloud && bootstrap

--- src/App.js
+++ src/App.js
@@ -1,19 +1,43 @@
-import OutputResult from './components/OutputResult';
-import UploadForm from './components/UploadForm';
-import React, {  useState } from "react";
+import OutputResult from "./components/OutputResult";
+import UploadForm from "./components/UploadForm";
+import React, { useMemo, useState } from "react";
+import { tokenizeWords } from "./hooks/nlp";
+import WordCloud from "react-d3-cloud";
+import { Container, Row, Col, Card } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  const [formValue, setFormValue] = useState(null);
+  const [formValue, setFormValue] = useState(" ");
+
+  const words = useMemo(() => tokenizeWords(formValue), [formValue]);
+  console.log(words);
+
+  const fontSizeMapper = (word) => Math.log2(word.value) / 10;
+
   return (
-    <div className="App">
-      <h1>Title</h1>
-      <h2>注册/登录</h2>
-      <UploadForm formValue={formValue} setFormValue={setFormValue} />
-      <OutputResult/>
-      <h2>
-        The files & text the user has uploaded
-      </h2>
-      <div>{formValue} </div>
+    <div>
+      <Container>
+        <h1>Title</h1>
+        <h2>注册/登录</h2>
+        <Row>
+          <UploadForm formValue={formValue} setFormValue={setFormValue} />
+        </Row>
+        <OutputResult />
+
+        {words.length != 0 && (
+          <WordCloud
+            data={words}
+            rotate={0}
+            height={100}
+            width={100}
+            padding={0}
+            font="Helvetica, sans-serif"
+          />
+        )}
+
+        <h2>The files & text the user has uploaded</h2>
+        <div>{formValue} </div>
+      </Container>
     </div>
   );
 }
--- src/components/UploadForm.js
+++ src/components/UploadForm.js
@@ -1,46 +1,21 @@
 import React, { useEffect, useState } from "react";
-
-function UploadForm({formValue, setFormValue}) {
+import { Button, Col, Container } from "react-bootstrap";
+function UploadForm({ formValue, setFormValue }) {
   const [fileInput, setFileInput] = useState(false);
-
+  const [file, setFile] = useState(null);
   const [textValue, setTextValue] = useState("");
-  const token ="ya29.a0AWY7Ckmop0n2UXpxq7Y2maHpKbnEJGItA94DzXcItQp6taAbD70Jat87p45YzY4ubg1g0G8L0yzuTf81N2q6nUfL0ilwSYYVHQdG7tjvzg8Hpo0hJqY9w_eFozCGI14jYpNplkvM1aI0AH6KnyRyyerOPCxsWxbiNX9lFQaCgYKAZASARMSFQG1tDrpzkr-lJixd6C5cvxQJA87SA0173";
- 
-   function gcpHandler(e) {
+
+  function gcpHandler(e) {
     e.preventDefault();
-    // async function runApi(){
-    // const gcpAPI = await fetch(
-    //   "https://us-central1-aiplatform.googleapis.com/v1/projects/my-project-nlp-23/locations/us-central1/publishers/google/models/text-bison@001:predict",
-    //   {
-    //     body: JSON.stringify({
-    //       instances: [
-    //         {
-    //           content:
-    //             "Provide a brief summary for the following article: " +
-    //             { formValue },
-    //         },
-    //       ],
-    //       parameters: {
-    //         temperature: 0.2,
-    //         maxOutputTokens: 768,
-    //         topP: 0.8,
-    //         topK: 40,
-    //       },
-    //     }),
-    //     headers: {
-    //       "Content-Type": "application/json",
-    //       Authorization: `Bearer ${token}`,
-    //     },
-    //     method: "POST",
-    //   }
-    // );
-    // console.log(gcpAPI.url);
-    // console.log(gcpAPI.response);
-    // console.log(gcpAPI);
-    // }
-    setFormValue(textValue);
-    setTextValue("");
-    console.log(formValue);
+
+    if (file) {
+      console.log(file);
+      setFormValue(file.name);
+      setFile(null);
+    } else {
+      setFormValue(textValue);
+      setTextValue("");
+    }
   }
 
   function formHandler(text) {
@@ -54,21 +29,31 @@ function UploadForm({formValue, setFormValue}) {
     setFileInput(false);
   }
 
+  function fileValueHandler(e) {
+    console.log(e.target.files);
+    setFile(e.target.files[0]);
+  }
 
   return (
     <div>
-      <button onClick={fileInputHandler}>text input</button>
-      <button onClick={textInputHandler}>file input</button>
+      <div class="mb-3">
+        <Button className="mt-auto" onClick={fileInputHandler}>
+          text input
+        </Button>
+        <Button onClick={textInputHandler}>file input</Button>
+      </div>
       <form onSubmit={gcpHandler}>
-        {fileInput ? (
-          <input type="file"></input>
-        ) : (
-          <textarea
-            value={textValue}
-            onChange={(e) => formHandler(e.target.value)}
-          ></textarea>
-        )}
-        <button>submit</button>
+        <div class="mb-3">
+          {fileInput ? (
+            <input type="file" onChange={fileValueHandler}></input>
+          ) : (
+            <textarea
+              value={textValue}
+              onChange={(e) => formHandler(e.target.value)}
+            ></textarea>
+          )}
+        </div>
+        <button class="btn btn-primary">submit</button>
       </form>
     </div>
   );
