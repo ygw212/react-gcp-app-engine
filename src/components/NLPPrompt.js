d6519255b2bd006ba5868d1af6bc963f153b14ab
ygw212 <wangygca@gmail.com>
2023-06-19 18:26:43 -0700

    Update NLPPrompt.js

--- src/components/NLPPrompt.js
+++ src/components/NLPPrompt.js
@@ -14,38 +14,7 @@ function NLPPrompt({setFormValue}) {
   let currentContent = useContext(ContentContext);
 
   function tokenHandler() {
-    axios
-      .post(
-        `https://oauth2.googleapis.com/revoke?token=${apiTOKEN}`,
-        
-        {
-          headers: {
-            // Overwrite Axios's automatically set Content-Type
-            "Content-Type": "application/x-www-form-urlencoded",
-          },
-        }
-      )
-      .then((res) => {
-        console.log(res);
-      })
-      .catch(function (error) {
-        if (error.response) {
-          // The request was made and the server responded with a status code
-          // that falls out of the range of 2xx
-          console.log(error.response.data);
-          console.log(error.response.status);
-          console.log(error.response.headers);
-        } else if (error.request) {
-          // The request was made but no response was received
-          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-          // http.ClientRequest in node.js
-          console.log(error.request);
-        } else {
-          // Something happened in setting up the request that triggered an Error
-          console.log("Error", error.message);
-        }
-        console.log(error.config);
-      });
+    
   }
 
   function nlpHanddler() {
